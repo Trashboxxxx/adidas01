24161e3294bfc21ff7e13d2981c81eb9bcca98cf
Trashboxxxx <wasuthornkrutsuwan@gmail.com>
2025-02-07 12:14:22 +0700

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,7 @@
+import 'package:adidas01/Adiclub.dart';
+import 'package:adidas01/Search.dart';
+import 'package:adidas01/Shopping.dart';
+import 'package:adidas01/favorite.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget{
@@ -10,6 +14,20 @@ class HomeScreen extends StatefulWidget{
 }
 
 class _HomeScreenState extends State<HomeScreen>{
+  int _selcetedIndex = 0;
+
+  final Map<int, Map<String,Widget>> _navigationMap = {
+    0: {"title": const Text("Home"),"screen": HomeScreen()},
+    1: {"title": const Text("Search"),"screen": Search()},
+    2: {"title": const Text("Favorite"),"screen": Favorite()},
+    3: {"title": const Text("Cart"),"screen": Shopping()},
+    4: {"title": const Text("Adiclub"),"screen": Adiclub()},
+  };
+  void OnItemTapped(int index){
+    setState(() {
+      _selcetedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +41,20 @@ class _HomeScreenState extends State<HomeScreen>{
             IconButton(icon: Icon(Icons.person_2_outlined, size: 45,),onPressed: (){},),
             IconButton(icon: Icon(Icons.settings, size: 45,),onPressed: (){},)
           ],
-          ),     
-          bottomNavigationBar: BottomNavigationBar(items:[
-            BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home')
-          ]),    
+          ),   
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _selcetedIndex,
+            onTap: OnItemTapped,
+            selectedItemColor: Colors.black,
+            unselectedItemColor: Colors.grey,
+            items: [
+              BottomNavigationBarItem(
+                icon: 
+                Image.asset("assets/icons/logo.png",width: 30,height: 30,),
+                label: ""),
+              BottomNavigationBarItem(icon: icon)
+            ],
+          ),      
       )
     );
   }
